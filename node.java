58e5ae2dfd65d63eeb6c31c5b952da02fa61cf68
Nesara-Munasinghe <133961203+nesara365@users.noreply.github.com>
2025-03-24 15:58:00 +0530

    Update node.java

---
+++ node.java
@@ -0,0 +1,11 @@
+public class node {
+    public Character letter;
+    public node left;
+    public node right;
+
+    public node(Character letter) {
+        this.letter = letter;
+        left = null;
+        right = null;
+    }
+}
